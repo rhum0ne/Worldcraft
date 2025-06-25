ffc5e85809cc38d049934a6e760f69b2880931c7
Romain <43304807+rhum0ne@users.noreply.github.com>
2025-06-26 01:46:17 +0200

    Load bone hierarchy from GLTF

--- src/main/java/fr/rhumun/game/worldcraftopengl/content/models/entities/GltfAnimationLoader.java
+++ src/main/java/fr/rhumun/game/worldcraftopengl/content/models/entities/GltfAnimationLoader.java
@@ -1,6 +1,7 @@
 package fr.rhumun.game.worldcraftopengl.content.models.entities;
 
 import fr.rhumun.game.worldcraftopengl.Game;
+import org.joml.Matrix4f;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
 import org.lwjgl.assimp.*;
@@ -24,6 +25,32 @@ public class GltfAnimationLoader {
 
         Map<String, Bone> bones = new HashMap<>();
         List<AnimationChannel<?>> channels = new ArrayList<>();
+
+        // Créer les bones à partir du mesh pour conserver les mêmes indices
+        if (scene.mNumMeshes() > 0) {
+            AIMesh mesh = AIMesh.create(scene.mMeshes().get(0));
+            PointerBuffer aiBones = mesh.mBones();
+            for (int i = 0; i < mesh.mNumBones(); i++) {
+                AIBone aiBone = AIBone.create(aiBones.get(i));
+                String name = aiBone.mName().dataString();
+                Bone bone = bones.computeIfAbsent(name, n -> new Bone(n, i));
+
+                Matrix4f offset = toMatrix(aiBone.mOffsetMatrix());
+                Matrix4f base = new Matrix4f(offset).invert();
+                Vector3f t = new Vector3f();
+                Quaternionf r = new Quaternionf();
+                Vector3f s = new Vector3f();
+                base.getTranslation(t);
+                base.getUnnormalizedRotation(r);
+                base.getScale(s);
+                bone.setTranslation(t);
+                bone.setRotation(r);
+                bone.setScale(s);
+            }
+        }
+
+        // Construction de la hiérarchie des bones
+        buildHierarchy(scene.mRootNode(), null, bones);
 
         AIAnimation animation = AIAnimation.create(scene.mAnimations().get(0)); // on ne traite que la première
 
@@ -70,5 +97,39 @@ public class GltfAnimationLoader {
 
         Assimp.aiReleaseImport(scene);
         return new Result(bones, channels);
+    }
+
+    private static Matrix4f toMatrix(AIMatrix4x4 aiMat) {
+        return new Matrix4f(
+                aiMat.a1(), aiMat.a2(), aiMat.a3(), aiMat.a4(),
+                aiMat.b1(), aiMat.b2(), aiMat.b3(), aiMat.b4(),
+                aiMat.c1(), aiMat.c2(), aiMat.c3(), aiMat.c4(),
+                aiMat.d1(), aiMat.d2(), aiMat.d3(), aiMat.d4()
+        );
+    }
+
+    private static void buildHierarchy(AINode node, Bone parent, Map<String, Bone> bones) {
+        String name = node.mName().dataString();
+        Bone current = bones.computeIfAbsent(name, n -> new Bone(n, bones.size()));
+        current.parent = parent;
+        if (parent != null) parent.children.add(current);
+
+        Matrix4f transform = toMatrix(node.mTransformation());
+        Vector3f t = new Vector3f();
+        Quaternionf r = new Quaternionf();
+        Vector3f s = new Vector3f();
+        transform.getTranslation(t);
+        transform.getUnnormalizedRotation(r);
+        transform.getScale(s);
+        current.setTranslation(t);
+        current.setRotation(r);
+        current.setScale(s);
+
+        if (node.mNumChildren() > 0) {
+            PointerBuffer children = node.mChildren();
+            for (int i = 0; i < node.mNumChildren(); i++) {
+                buildHierarchy(AINode.create(children.get(i)), current, bones);
+            }
+        }
     }
 }
