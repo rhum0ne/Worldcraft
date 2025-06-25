4584a2bb46659f872d49cf843a24cb64e8d9a578
Romain <43304807+rhum0ne@users.noreply.github.com>
2025-06-25 11:23:51 +0200

    Added a scroll action in Worlds Menu

--- src/main/java/fr/rhumun/game/worldcraftopengl/outputs/graphic/guis/types/worlds_menu/LoadWorldButton.java
+++ src/main/java/fr/rhumun/game/worldcraftopengl/outputs/graphic/guis/types/worlds_menu/LoadWorldButton.java
@@ -11,18 +11,18 @@ import static fr.rhumun.game.worldcraftopengl.Game.GAME;
 public class LoadWorldButton extends Button {
 
     private final WorldInfo info;
-    private static final int VISIBLE_TOP = -100;
-    private static final int VISIBLE_BOTTOM = 200;
+    public static final int VISIBLE_TOP = -100;
+    private static final int VISIBLE_BOTTOM = 220;
 
     public LoadWorldButton(int x, int y, Gui container, WorldInfo info) {
-        super(x, y, container, info.name());
+        super(x, y, 400, 40, container, info.name());
         this.info = info;
     }
 
     @Override
     public void render() {
-        int localY = (getY() - (getContainer().getHeight() / 2 - getHeight() / 2)) / Game.GUI_ZOOM;
-        boolean visible = localY + getHeight() / Game.GUI_ZOOM > VISIBLE_TOP && localY < VISIBLE_BOTTOM;
+        int localY = (getY() - (getContainer().getHeight() / 4 - getHeight() / 2)) / Game.GUI_ZOOM;
+        boolean visible = localY > VISIBLE_TOP && localY < VISIBLE_BOTTOM;
         setVisible(visible);
         if (visible) {
             super.render();
--- src/main/java/fr/rhumun/game/worldcraftopengl/outputs/graphic/guis/types/worlds_menu/WorldsGui.java
+++ src/main/java/fr/rhumun/game/worldcraftopengl/outputs/graphic/guis/types/worlds_menu/WorldsGui.java
@@ -9,6 +9,8 @@ import fr.rhumun.game.worldcraftopengl.worlds.WorldInfo;
 import java.util.ArrayList;
 import java.util.List;
 
+import static fr.rhumun.game.worldcraftopengl.outputs.graphic.guis.types.worlds_menu.LoadWorldButton.VISIBLE_TOP;
+
 public class WorldsGui extends FullscreenTiledGui implements ScrollableGui {
 
     private final List<LoadWorldButton> worldButtons = new ArrayList<>();
@@ -21,7 +23,7 @@ public class WorldsGui extends FullscreenTiledGui implements ScrollableGui {
 
         this.addText(0, -200, "Liste des Mondes");
 
-        int y = -100;
+        int y = VISIBLE_TOP;
         List<WorldInfo> infos = SaveManager.listWorldInfos();
         for (WorldInfo info : infos) {
             LoadWorldButton btn = new LoadWorldButton(0, y, this, info);
@@ -30,11 +32,11 @@ public class WorldsGui extends FullscreenTiledGui implements ScrollableGui {
             y += 50;
         }
 
-        createButton = new CreateWorldButton(0, 150, this);
-        this.addButton(createButton);
-
-        backButton = new BackButton(0, 210, this);
+        backButton = new BackButton(100, 210, this);
         this.addButton(backButton);
+
+        createButton = new CreateWorldButton(-100, 210, this);
+        this.addButton(createButton);
 
         this.setAlignCenter(true);
 
@@ -44,13 +46,18 @@ public class WorldsGui extends FullscreenTiledGui implements ScrollableGui {
     @Override
     public void onScroll(double yoffset) {
         scrollOffset += yoffset > 0 ? 20 : -20;
+
+        if(scrollOffset > 0) scrollOffset = 0;
+        else if(scrollOffset < -50*worldButtons.size()) scrollOffset = -50*worldButtons.size();
+
         updatePositions();
     }
 
     private void updatePositions() {
-        int y = -100 + scrollOffset;
+        int y = VISIBLE_TOP + scrollOffset;
         for (LoadWorldButton btn : worldButtons) {
             setCoordinates(btn, 0, y);
+            btn.getText().set2DCoordinates(0,0);
             y += 50;
         }
     }
